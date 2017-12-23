46f14d3f1ca1691f3ce5bc77a2aa59e4a5062c9c
William Castelli (Billy) <billycastelli@gmail.com>
2017-12-22 23:20:14 -0800

    Update stock.py
    Added ability to load symbols from file and save session's symbols to file.

--- stock.py
+++ stock.py
@@ -5,11 +5,15 @@ ALL = {}
 #note: add date to top
 
 def enter_name():
-    return input("Enter a stock symbol: ")
+    return input("Enter a stock symbol (press enter to end program): ")
 
 def adder(symbol = "Holder"):
+    # "holder" is a placeholder
     if symbol == "Holder":
         symbol = enter_name().upper()
+    if symbol == "":
+        toFile()
+        #return
     url = stock_api.create_url(symbol)
     #print(url)
     try:
@@ -42,15 +46,44 @@ def display(ALL: dict):
     #print(data)
     return
 
+def fromFile():
+    response = input("Read stocks from file? (y/n) ")
+    if response[0].lower() == 'y':
+        fileName = input("Enter file name: ")
+        try:
+            file = open(fileName)
+        except:
+            print("File not found")
+            return
+        print("Loading...")
+        for symbol in file:
+            adder(symbol.strip())
+        display(ALL)
+    else:
+        print("Starting program...")
+        return
+    return
 
-    
-
-
+def toFile():
+    response = input("Would you like to save this session's symbols? ")
+    fileName = input("Enter file name: ")
+    if response[0].lower() == 'y':
+        file = open(fileName, 'w')
+        for (key,value) in ALL.items():
+            file.write(key + '\n')
+        file.close()
+    print("Symbols saved as symbolsFile.txt")
+    print("Ending program...")
+    exit()
 
 if __name__ == "__main__":
     #add updating using time modules (update every minute)
+    fromFile()
     while True:
         adder()
         update(ALL)
         display(ALL)
+
+#load from last session vs. load from file
+    
  
